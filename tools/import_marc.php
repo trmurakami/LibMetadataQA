e8812832800f079b41b0f354fbe0f3ffc23eab48
Tiago <tiago@bdpife2.sibi.usp.br>
2019-10-09 14:29:54 -0300

    Initial

--- tools/import_marc.php
+++ tools/import_marc.php
@@ -2,17 +2,18 @@
 
 require '../inc/functions.php';
 
-if (isset($_REQUEST["marcFormat"])) {
-    if (isset($_FILES['fileMARC'])) {
+if (isset($_REQUEST["marcFormat"])) {    
+    if (isset($_FILES['fileMARC'])) {      
         
         if (file_exists("../data/marc.json")) {
             unlink("../data/marc.json");
-        }
+        }       
+
         if (move_uploaded_file($_FILES['fileMARC']['tmp_name'], "../data/marc.seq")) {
             shell_exec('catmandu convert MARC '.$_REQUEST["marcFormat"].' to MARC --type MiJ < ../data/marc.seq > ../data/marc.json');
             shell_exec('uconv -f utf-8 -t latin1 < ../data/marc.json > ../data/marcconv.json');
-            unlink("../data/marc.seq");            
-            unlink("../data/marc.json");       
+            unlink("../data/marc.seq");
+            unlink("../data/marc.json");
 
             if (file_exists("../data/marcconv.json")) {
                 $marc_in_json = file("../data/marcconv.json");
